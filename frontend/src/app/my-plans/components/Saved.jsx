8349c83074c129fd3df2aaa95d16e61f1b1ee985
SBeeeee <shatadru017@gmail.com>
2025-06-27 00:17:20 +0530

    added needed imports

--- frontend/src/app/my-plans/components/Saved.jsx
+++ frontend/src/app/my-plans/components/Saved.jsx
@@ -1,6 +1,7 @@
+"use client"
 import React from 'react'
 import TravelCard from '@/app/AllRides/components/TravelCard'
-
+import { Wrench, AlertCircle, Loader2 } from "lucide-react";
 
 export default function Saved() {
   return (
