32312a8a81612e322fa08de5e91ef037f4c3e18d
SBeeeee <shatadru017@gmail.com>
2025-06-26 16:38:43 +0530

    removed production bug

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,14 +1,13 @@
 "use client"
 import HeroSection from '@/components/HeroSection';
 import HowItWorks from '@/components/HowItWorks';
-import Navbar from '@/components/Navbar';
+
 import React from 'react'
-import { useSelector } from 'react-redux'
+
 
 
 export default function page() {
-  const user = useSelector((state) => state.auth.user);
-  console.log(user)
+ 
   return (
     <div>
      
